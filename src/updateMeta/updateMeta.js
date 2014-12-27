de984796cf1743e67a832e56ab7123599401aa97
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-12-27 09:26:17 +0100

    feat(updateMeta): add module

---
+++ src/updateMeta/updateMeta.js
@@ -0,0 +1,6 @@
+// Create all modules and define dependencies to make sure they exist
+// and are loaded in the correct order to satisfy dependency injection
+// before all nested files are concatenated by Gulp
+
+// Modules
+angular.module('updateMeta', []);
---
+++ test/unit/updateMeta/updateMetaSpec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('', function() {
+
+  var module;
+
+  beforeEach(function() {
+    // Get module
+    module = angular.module('updateMeta');
+  });
+
+  it('should exist', function() {
+    expect(module).to.be.ok;
+  });
+
+});
