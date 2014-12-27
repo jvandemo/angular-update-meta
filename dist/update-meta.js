5671d864096a6ecaeb7b379a6b2022925c0f135e
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-12-27 09:26:49 +0100

    feat(dist): update build

---
+++ dist/update-meta.js
@@ -0,0 +1,56 @@
+// Create all modules and define dependencies to make sure they exist
+// and are loaded in the correct order to satisfy dependency injection
+// before all nested files are concatenated by Gulp
+
+// Modules
+angular.module('updateMeta', []);
+
+(function(){
+
+  /**
+   * Update a meta tag dynamically
+   *
+   * See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta
+   *
+   * @constructor
+   */
+  function UpdateMetaDirective(){
+    return {
+      restrict: 'E',
+      scope: {
+        charset: '@',
+        name: '@',
+        content: '@',
+        httpEquiv: '@',
+        scheme: '@'
+      },
+      link: function(scope, iElem, iAttrs){
+
+        if(scope.name && scope.content){
+          angular.element('meta[name=' + scope.name + ']').attr('content', scope.content);
+          return;
+        }
+
+        if(scope.httpEquiv && scope.content){
+          angular.element('meta[httpEquiv=' + scope.name + ']').attr('content', scope.content);
+          return;
+        }
+
+        if(scope.charset){
+          angular.element('meta[charset]').attr('charset', scope.charset);
+          return;
+        }
+
+      }
+    };
+  }
+
+  // Inject dependencies
+  UpdateMetaDirective.$inject = [];
+
+  // Export
+  angular
+    .module('updateMeta')
+    .directive('updateMeta', UpdateMetaDirective);
+
+})();
