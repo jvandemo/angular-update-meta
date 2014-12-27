2122c556683cac32bfbbc195fb1586c1bbe32089
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-12-27 09:25:20 +0100

    feat(gulp): add gulpfile with build tasks

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    karma = require('karma').server,
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sourceFiles = [
+      'src/updateMeta/updateMeta.js',
+      'src/updateMeta/directives/**/*.js',
+    ];
+
+gulp.task('build', function() {
+  gulp.src(sourceFiles)
+    .pipe(concat('update-meta.js'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(uglify())
+    .pipe(rename('update-meta.min.js'))
+    .pipe(gulp.dest('./dist'))
+});
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', ['test-dist-concatenated', 'test-dist-minified']);
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test-dist-concatenated', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma-dist-concatenated.conf.js',
+    singleRun: true
+  }, done);
+});
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test-dist-minified', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma-dist-minified.conf.js',
+    singleRun: true
+  }, done);
+});
+
+gulp.task('default', ['build']);
