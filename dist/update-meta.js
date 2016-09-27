2d043e4301997c6a66926e362433633dbe5219d0
khisakuni <kohei@gophilosophie.com>
2016-09-26 21:59:31 -0400

    Add support for other link attributes

--- dist/update-meta.js
+++ dist/update-meta.js
@@ -14,20 +14,67 @@ angular.module('updateMeta', []);
    */
   function UpdateLinkDirective($log, updateAttribute) {
 
+    var supportedAttributes =
+      [
+        'href',
+        'charset',
+        'crossorigin',
+        'hreflang',
+        'integrity',
+        'media',
+        'methods',
+        'referrerpolicy',
+        'sizes',
+        'target',
+        'title',
+        'type'
+      ];
+
+      var queryAttributes = ['id', 'rel'];
+
+      function buildAttributeFilter(scope, attribute) {
+        return '[' + attribute + '="' + scope[attribute] + '"]';
+      };
+
+      function buildQuery(scope, attributes) {
+        return attributes.filter(function(attribute) {
+          return scope[attribute];
+        }).map(function(attribute) {
+          return buildAttributeFilter(scope, attribute);
+        }).join('');
+      };
+
     return {
       restrict: 'E',
       scope: {
         rel: '@',
-        href: '@'
+        href: '@',
+        id: '@?',
+        charset: '@?',
+        crossorigin: '@?',
+        hreflang: '@?',
+        integrity: '@?',
+        media: '@?',
+        methods: '@?',
+        referrerpolicy: '@?',
+        sizes: '@?',
+        target: '@?',
+        title: '@?',
+        type: '@?'
       },
       link: function(scope, iElem, iAttrs) {
-        var selector = 'link[rel="' + scope.rel + '"]';
 
-        // watch the content parameter and set the changing value as needed
-        scope.$watch('href', function (newValue, oldValue) {
-          if (typeof newValue !== 'undefined') {
-            updateAttribute.update(selector, 'href', scope.href);
-          }
+        var selector = 'link' + buildQuery(scope, supportedAttributes);
+
+        // Watch only desired attributes
+        supportedAttributes.filter(function(attribute) {
+          return scope[attribute];
+        }).forEach(function(attribute) {
+          scope.$watch(attribute, function(newValue, oldValue) {
+            if (typeof newValue !== 'undefined') {
+              updateAttribute.update(selector, attribute, scope[attribute])
+            }
+          });
         });
       }
     };
